910760ebd39ddb4c4a884a938642b25dfe667382
Ninowis <guillaume@ninowis.com>
2019-02-22 21:02:14 +0000

    Add Sass toolchain with post CSS processing, minification in production and source maps in dev. Add webfonts and favicon assets management. Setup Atomic Design based Sass architecture.

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # React App Boilerplate
 A React + Webpack stack to kick start front-end developments.
 
+
 ## Pre-requisites
 
 * [Node.js](https://nodejs.org/): Node >= 6 and npm >= 5.2 on your machine
 * (Optional) [Yarn](https://yarnpkg.com/en/docs/install): for an optimised npm package management
 
+
 ## Features
 
 * Webpack 4 + Babel 7 toolchain
+* Atomic Design based Sass architecture (with added config and Vendor libs management), compiling into external stylesheet rather than into DOM / JS modules
+* Post CSS processing including autoprefixing with [PostCSS Preset Env](https://github.com/csstools/postcss-preset-env) to convert modern CSS into something most browsers can understand, determining the polyfills you need based on your targeted browsers or runtime environments.
+* CSS minification in production mode using [cssnano](https://cssnano.co/)
+* Source Maps and Hot Reload when running in development mode (using [webpack-dev-server](https://github.com/webpack/webpack-dev-server))
+* Webfonts assets toolchain
 
 
 ## Getting Started
@@ -19,3 +26,18 @@ A React + Webpack stack to kick start front-end developments.
 
 The page will reload if you make edits.<br>
 You will also see any lint errors in the console.
+
+
+## Available Scripts
+
+In the project directory, you can also run:
+
+### `npm run build`
+
+Builds the app for production to the `dist` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,83 @@
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode,
+    entry: [
+      './src/index.js',
+      './src/sass/app-styles.scss'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'styles.css',
+                outputPath: 'css/',
+                publicPath: '../'
+              }
+            },
+            { loader: 'extract-loader' },
+            { loader: 'css-loader', options: { sourceMap: (argv.mode === 'development') } },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: (argv.mode === 'development'),
+                plugins: () => [
+                  require('postcss-preset-env'),
+                  require('cssnano')
+                ]
+              }
+            },
+            { loader: 'sass-loader', options: { sourceMap: (argv.mode === 'development') } }
+          ]
+        },
+        {
+          test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[folder]/[name].[ext]",
+              outputPath: './fonts/',
+              publicPath: '../fonts/'
+            }
+          }
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "./",
+      filename: "main.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      port: 3000,
+      publicPath: "http://localhost:3000/",
+      hotOnly: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new CopyWebpackPlugin([
+        { from: './public/favicon.ico' }
+      ]),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        filename: 'index.html'
+      })
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "main.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
